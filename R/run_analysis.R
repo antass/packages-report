b579a37f614d41b12ff79f2f9621fef5bbafd439
ania.tassinari <ania.tassinari@agios.com>
2019-01-15 17:15:36 -0600

    Copy file over from Jenny's repo

---
+++ R/run_analysis.R
@@ -0,0 +1,8 @@
+# Get / make data
+source(here("R/01_write-installed-packages.R"))
+
+# Clean data
+source(here("R/02_wrangle-packages.R"))
+
+# Make bargraph
+source(here("R/03_barchart-packages-built.R"))
--- test.R
+++
@@ -1,9 +0,0 @@
-#' ---
-#' output: github_document
-#' ---
-
-
-.libPaths()
-
-ipt <- installed.packages()
-nrow(ipt)
